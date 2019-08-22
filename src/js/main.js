a8acc023f3e04178d10586dfaeced73e3af367db
mazax1ct <vova@giveandget.ru>
2019-08-22 13:30:20 +0300

    products-slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //открытие/закрытие меню
   $('.js-menu-opener').click(function () {
     $('body').toggleClass('overflow');
     $(this).toggleClass('is-active');
@@ -26,6 +27,44 @@ $(document).ready(function () {
       prevArrow: '<button type="button" class="slick-prev slick-arrow" title="Назад"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#arrow_left"/></svg></button>',
       nextArrow: '<button type="button" class="slick-next slick-arrow" title="Вперед"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#arrow_right"/></svg></button>',
       dots: false
+    });
+  }
+
+  //слайдер товаров
+  if ($('.js-products-slider').length) {
+    $('.js-products-slider').slick({
+      auto: false,
+      mobileFirst: true,
+      slidesToShow: 1,
+      infinite: true,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev slick-arrow" title="Назад"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#arrow_left"/></svg></button>',
+      nextArrow: '<button type="button" class="slick-next slick-arrow" title="Вперед"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#arrow_right"/></svg></button>',
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 559,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 639,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            appendArrows: '.js-products-slider-nav'
+          }
+        }
+      ]
     });
   }
 });
